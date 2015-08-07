d2c0ff55675a1e49956477ece4772acce46d0ee2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-08-07 16:50:24 +0200

    work

--- test-openal-wrapper/main.cpp
+++ test-openal-wrapper/main.cpp
@@ -4,65 +4,104 @@
 #include <chrono>
 #include <thread>
 
-oalplus::ALUtilityToolkit* alut{};
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
 class Sound
 {
     public:
         Sound():
-            m_source{oalplus::ObjectDesc("ASource")}
+            Sound{oalplus::ALUtilityToolkit(false).CreateBufferHelloWorld()}
         {
-            // create a Hello World sound and store it into a buffer
-            oalplus::Buffer buffer = alut->CreateBufferHelloWorld();
+        }
 
-            // create a source from the data in buffer and set enable looping
-            m_source = oalplus::Source(oalplus::ObjectDesc("Source"));
-            m_source.Buffer(buffer);
+        Sound(const std::string& filename):
+            Sound{oalplus::ALUtilityToolkit(false).CreateBufferFromFile(filename)}
+        {
+        }
+
+        Sound(const oalplus::Buffer& buf):
+            m_source{oalplus::ObjectDesc("Source")}
+        {
+            m_source.Buffer(buf);
             m_source.Looping(true);
             m_source.ReferenceDistance(15);
         }
 
         auto& source()
-        { return m_source; }
+        {
+            return m_source;
+        }
+
+        const auto& source() const
+        {
+            return m_source;
+        }
 
         void setEnabled(bool)
         {
 
         }
 
+        const std::string& name() const
+        {
+            return m_name;
+        }
+
+        void setName(const std::string &name)
+        {
+            m_name = name;
+        }
+
     private:
+        std::string m_name;
         oalplus::Source m_source;
 };
 
+namespace bmi = boost::multi_index;
+using SoundMap = bmi::multi_index_container<
+    Sound,
+    bmi::indexed_by<
+        bmi::hashed_unique<
+            bmi::const_mem_fun<
+                Sound,
+                const std::string&,
+                &Sound::name
+            >
+        >
+    >
+>;
 class Scene
 {
     public:
         Scene()
         {
             // create a listener and set its position, velocity and orientation
-            listener.Position(0.0f, 0.0f, 0.0f);
-            listener.Velocity(0.0f, 0.0f, 0.0f);
-            listener.Orientation(0.0f, 0.0f,-1.0f, 0.0f, 1.0f, 0.0f);
+            m_listener.Position(0.0f, 0.0f, 0.0f);
+            m_listener.Velocity(0.0f, 0.0f, 0.0f);
+            m_listener.Orientation(0.0f, 0.0f,-1.0f, 0.0f, 1.0f, 0.0f);
 
-            m_sounds.resize(1);
-
+            m_sounds.insert(Sound());
         }
 
         void start()
         {
-            for(auto& sound : m_sounds)
+            for(auto& cst_sound : m_sounds)
             {
+                auto& sound = const_cast<Sound&>(cst_sound);
                 auto& source = sound.source();
                 // let the source play the sound
                 source.Play();
 
                 // the path of the source
-                oalplus::CubicBezierLoop<oalplus::Vec3f, double> path({
-                                                                          oalplus::Vec3f( 0.0f, 0.0f, -9.0f),
-                                                                          oalplus::Vec3f( 9.0f, 2.0f, -8.0f),
-                                                                          oalplus::Vec3f( 4.0f,-3.0f, 9.0f),
-                                                                          oalplus::Vec3f(-3.0f, 4.0f, 9.0f),
-                                                                          oalplus::Vec3f(-9.0f,-1.0f, -7.0f)
-                                                                      });
+                oalplus::CubicBezierLoop<oalplus::Vec3f, double> path(
+                {
+                                oalplus::Vec3f( 0.0f, 0.0f, -9.0f),
+                                oalplus::Vec3f( 9.0f, 2.0f, -8.0f),
+                                oalplus::Vec3f( 4.0f,-3.0f, 9.0f),
+                                oalplus::Vec3f(-3.0f, 4.0f, 9.0f),
+                                oalplus::Vec3f(-9.0f,-1.0f, -7.0f)
+                            });
                 //
                 // play the sound for a while
                 typedef std::chrono::system_clock clock;
@@ -71,11 +110,11 @@ class Scene
                 while(true)
                 {
                     double t = double((clock::now() - start).count())*
-                               double(clock::period::num)/
-                               double(clock::period::den);
+                            double(clock::period::num)/
+                            double(clock::period::den);
                     if(t > 10.0) break;
                     source.Position(path.Position(t/5.0));
-                    listener.Position(listener.Position() - oalplus::Vec3f{0.2, 0, 0});
+                    m_listener.Position(m_listener.Position() - oalplus::Vec3f{0.2, 0, 0});
                     // wait for a while
                     std::chrono::milliseconds duration(10);
                     std::this_thread::sleep_for(duration);
@@ -83,31 +122,153 @@ class Scene
             }
         }
 
-        auto& listener()
+        oalplus::Listener& listener()
         { return m_listener; }
 
+        SoundMap& sounds()
+        { return m_sounds; }
+
     private:
-        oalplus::Listener listener;
-        std::vector<Sound> m_sounds;
+        oalplus::Listener m_listener;
+        SoundMap m_sounds;
 };
+
+#include <Network/Device.h>
+#include <Network/Protocol.h>
+#include <Network/Node.h>
+
+
+
+template<typename Get, typename Set>
+struct Parameter
+{
+        Get get;
+        Set set;
+};
+
+
+template<typename Get, typename Set>
+auto make_parameter(Get&& get, Set&& set)
+{
+    return Parameter<Get, Set>{std::move(get), std::move(set)};
+}
+
+template<typename Parameter_t> // a function that returns a Vec3f& to modify.
+void add_position(std::shared_ptr<OSSIA::Node> node, Parameter_t&& param)
+{
+    auto addr = node->createAddress(OSSIA::Value::Type::TUPLE); // [ x y z ]
+    auto x_node = *node->emplace(node->children().cend(), "x");
+    auto x_addr = x_node->createAddress(OSSIA::Value::Type::FLOAT);
+    auto y_node = *node->emplace(node->children().cend(), "y");
+    auto y_addr = y_node->createAddress(OSSIA::Value::Type::FLOAT);
+    auto z_node = *node->emplace(node->children().cend(), "z");
+    auto z_addr = z_node->createAddress(OSSIA::Value::Type::FLOAT);
+
+    addr->setValueCallback([=] (const OSSIA::Value* val) {
+        auto tpl = dynamic_cast<const OSSIA::Tuple*>(val);
+        if(tpl->value.size() != 3)
+            return;
+
+        auto x = dynamic_cast<const OSSIA::Float*>(tpl->value[0]);
+        auto y = dynamic_cast<const OSSIA::Float*>(tpl->value[1]);
+        auto z = dynamic_cast<const OSSIA::Float*>(tpl->value[2]);
+        if(!x || !y || !z)
+            return;
+
+        param.set(oalplus::Vec3f{x->value, y->value, z->value});
+    });
+
+    auto modify_i = [=] (int i) {
+        return [=] (const OSSIA::Value* val) {
+            auto float_val = dynamic_cast<const OSSIA::Float*>(val);
+            if(!float_val)
+                return;
+
+            auto elt = param.get();
+            elt[i] = float_val->value;
+            param.set(elt);
+        };
+    };
+
+    x_addr->setValueCallback(modify_i(0));
+    y_addr->setValueCallback(modify_i(1));
+    z_addr->setValueCallback(modify_i(2));
+}
+
+template<typename VecFun> // a function that takes a Vec6f& to modify.
+void add_orientation(std::shared_ptr<OSSIA::Node> node, VecFun&& vecfun)
+{
+    auto addr = node->createAddress(OSSIA::Value::Type::TUPLE); // [ at_x at_y at_z up_x at_y at_z ]
+    auto at_node = *node->emplace(node->children().cend(), "at");
+    add_position(at_node, [&] () { return vecfun(); } );
+    auto up_node = *node->emplace(node->children().cend(), "up");
+}
 
 int main(int argc, char** argv)
 {
-    // open the default device
     oalplus::Device device;
-    // create a context using the device and make it current
     oalplus::ContextMadeCurrent context(
                 device,
-                oalplus::ContextAttribs()
-                .Add(oalplus::ContextAttrib::MonoSources, 1)
-                .Get()
-                );
-    // create an instance of ALUT
-    alut = new oalplus::ALUtilityToolkit(false, argc, argv);
+                oalplus::ContextAttribs().Add(oalplus::ContextAttrib::MonoSources, 1).Get());
 
     Scene s;
+
+    OSSIA::Local localDeviceParameters{};
+    auto localDevice = OSSIA::Device::create(localDeviceParameters, "3DAudioScene");
+    {
+        // Listener : position, orientation, volume ?
+
+        // Sources : position, orientation, volume, enabled, sound file ?
+
+        //// Global settings ////
+        auto settings_node = *localDevice->emplace(localDevice->children().cend(), "settings");
+        auto vol_node = settings_node->emplace(settings_node->children().cend(), "volume");
+        auto files_node = settings_node->emplace(settings_node->children().cend(), "files");
+
+        auto vol_addr = (*vol_node)->createAddress(OSSIA::Value::Type::FLOAT);
+        auto files_addr = (*files_node)->createAddress(OSSIA::Value::Type::TUPLE);
+
+        vol_addr->setValueCallback([] (const OSSIA::Value* val) { });
+        files_addr->setValueCallback([] (const OSSIA::Value* val) { });
+
+        // updating local tree value
+        //OSSIA::Bool b(true);
+        //localTestAddress->sendValue(&b);
+
+        //// Listener settings ////
+        auto listener_node = *localDevice->emplace(localDevice->children().cend(), "listener");
+
+        auto listener_pos_node = *listener_node->emplace(listener_node->children().cend(), "pos");
+
+        add_position(listener_pos_node,
+                     make_parameter(
+                         [&] () { return s.listener().Position(); },
+                         [&] (const auto& elt) { s.listener().Position(elt); }
+                     ));
+
+        auto listener_orient_node = *listener_node->emplace(listener_node->children().cend(), "orient");
+
+        //auto addr = listener_orient_node->createAddress(OSSIA::Value::Type::TUPLE); // [ at_x at_y at_z up_x at_y at_z ]
+        auto listener_orient_at_node = *listener_orient_node->emplace(listener_orient_node->children().cend(), "at");
+        add_position(listener_orient_at_node,
+                     make_parameter(
+                         [&] () { return s.listener().OrientationAt(); },
+                         [&] (const auto& elt) { s.listener().Orientation(elt, s.listener().OrientationUp()); }
+                     ));
+        auto listener_orient_up_node = *listener_orient_node->emplace(listener_orient_node->children().cend(), "up");
+        add_position(listener_orient_up_node,
+                     make_parameter(
+                         [&] () { return s.listener().OrientationUp(); },
+                         [&] (const auto& elt) { s.listener().Orientation(s.listener().OrientationAt(), elt); }
+                     ));
+
+    }
     s.start();
 
     //
     return 0;
 }
+
+
+
+
